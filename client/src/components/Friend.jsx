8a2642a3b4cc8c9709e25956fe160c14422746c6
PRASHANTHANSDA2002 <110048717+PRASHANTHANSDA2002@users.noreply.github.com>
2023-07-02 10:13:07 +0530

    Update Friend.jsx

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -22,7 +22,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
 
   const patchFriend = async () => {
     const response = await fetch(
-        `https://socialmediaoverflow.onrender.com/${_id}/${friendId}`,
+        `https://socialmediaoverflow.onrender.com/users/${_id}/${friendId}`,
         {
           method: "PATCH",
           headers: {
